78af4cf5191b648bdd49ef7355b12bd7408a93af
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-06-20 17:16:48 +0900

    [Setting] #1- 코드베이스 프로젝트 세팅

---
+++ Terning-iOS/Terning-iOS/Applicatoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Terning-iOS
+//
+//  Created by 이명진 on 6/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+
+
+}
+

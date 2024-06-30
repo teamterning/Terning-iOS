1768c2d7d8e68aa2e746fce570fab72513bba64d
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-06-30 16:12:54 +0900

    [Add] #3 - 그림자 Extension 추가

---
+++ Terning-iOS/Terning-iOS/Resource/Extension/CALayer+.swift
@@ -0,0 +1,41 @@
+//
+//  CALayer+.swift
+//  Terning-iOS
+//
+//  Created by 이명진 on 6/29/24.
+//
+
+import UIKit
+
+/**
+ 
+ - Description:
+ 
+ View의 Layer 계층(CALayer)에 shadow를 간편하게 입힐 수 있는 메서드입니다.
+ 피그마에 나와있는 shadow 속성 값을 그대로 기입하면 됩니다!
+ 
+ */
+
+public extension CALayer {
+    func applyShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.05, // 피그마 디자인 시스템에 있는 기본 alpha 값 입니다.
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 5,
+        spread: CGFloat = 0) {
+            
+            masksToBounds = false
+            shadowColor = color.cgColor
+            shadowOpacity = alpha
+            shadowOffset = CGSize(width: x, height: y)
+            shadowRadius = blur / 2.0
+            if spread == 0 {
+                shadowPath = nil
+            } else {
+                let dx = -spread
+                let rect = bounds.insetBy(dx: dx, dy: dx)
+                shadowPath = UIBezierPath(rect: rect).cgPath
+            }
+        }
+}

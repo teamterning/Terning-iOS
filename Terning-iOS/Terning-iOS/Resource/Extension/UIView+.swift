16488e3c960b7286bfc45b2e283f808c404f528b
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-07-07 16:15:25 +0900

    [Feat] #10 - UIView/UIViewController + 햅틱 추가

--- Terning-iOS/Terning-iOS/Resource/Extension/UIView+.swift
+++ Terning-iOS/Terning-iOS/Resource/Extension/UIView+.swift
@@ -14,4 +14,31 @@ extension UIView {
         views.forEach { self.addSubview($0) }
     }
     
+    /// width와 color를 받아 UIView의 Border를 설정합니다.
+    func makeBorder(width: CGFloat, color: UIColor) {
+        layer.borderWidth = width
+        layer.borderColor = color.cgColor
+    }
+}
+
+extension UIView {
+    
+    /**
+     
+     - Description:
+     
+     VC나 View 내에서 해당 함수를 호출하면, 햅틱이 발생하는 메서드입니다.
+     버튼을 누르거나 유저에게 특정 행동이 발생했다는 것을 알려주기 위해 다음과 같은 햅틱을 활용합니다.
+     
+     - parameters:
+     
+     - degree: 터치의 세기 정도를 정의합니다. 보통은 medium,light를 제일 많이 활용합니다?!
+     따라서 파라미터 기본값을 . medium으로 정의했습니다.
+     
+     */
+    
+    func makeVibrate(degree: UIImpactFeedbackGenerator.FeedbackStyle = .medium) {
+        let generator = UIImpactFeedbackGenerator(style: degree)
+        generator.impactOccurred()
+    }
 }

7eb10a53083605ce81ad0b244e19852b458c9237
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-07-08 02:49:06 +0900

    [Setting] #10 - 이미지 URL로 받아오는 기능 구현

---
+++ Terning-iOS/Terning-iOS/Network/Base/BaseResponse.swift
@@ -0,0 +1,19 @@
+//
+//  BaseResponse.swift
+//  Terning-iOS
+//
+//  Created by 이명진 on 7/8/24.
+//
+
+import Foundation
+
+struct BaseResponse<T: Codable>: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: T?
+}
+
+/// data가 없는 API 통신에서 사용할 BlankData 구조체
+struct BlankData: Codable {
+}

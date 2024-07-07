835486d6a4066182f18bc748018d2573d642433a
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-07-08 06:57:27 +0900

    [Fix] #10 - 1차 코드 리뷰 적용
    - setBackgroundColor

--- Terning-iOS/Terning-iOS/Resource/Extension/UIKit+/UIButton+.swift
+++ Terning-iOS/Terning-iOS/Resource/Extension/UIKit+/UIButton+.swift
@@ -10,17 +10,13 @@ import UIKit
 extension UIButton {
     /// 버튼 Background Color를 상태별로 지정하는 메서드
     func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
-        let minimumSize: CGSize = CGSize(width: 1.0, height: 1.0)
+        let minimumSize = CGSize(width: 1.0, height: 1.0)
         
-        UIGraphicsBeginImageContext(minimumSize)
-        
-        if let context = UIGraphicsGetCurrentContext() {
-            context.setFillColor(color.cgColor)
+        let renderer = UIGraphicsImageRenderer(size: minimumSize)
+        let colorImage = renderer.image { context in
+            color.setFill()
             context.fill(CGRect(origin: .zero, size: minimumSize))
         }
-        
-        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
         
         self.clipsToBounds = true
         self.setBackgroundImage(colorImage, for: state)

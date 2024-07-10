4d0dec5ac4dabb578c33be12ad0bcf55ebb40db8
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-07-11 02:31:42 +0900

    [Chore] #25 - rootViewController 수정

--- Terning-iOS/Terning-iOS/Application/SceneDelegate.swift
+++ Terning-iOS/Terning-iOS/Application/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let vc = ProfileViewController(viewType: .fix, viewModel: ProfileViewModel())
+        let vc = TNTabBarController()
         window.rootViewController = vc
         self.window = window
         window.makeKeyAndVisible()

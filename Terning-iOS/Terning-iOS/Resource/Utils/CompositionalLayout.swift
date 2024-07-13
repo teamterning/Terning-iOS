4b498b6027ca2f5a5b626226419d9cec44710d30
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-07-14 05:33:58 +0900

    [Feat] #43 - 캘린더 리스트 뷰 Compositional 적용

--- Terning-iOS/Terning-iOS/Resource/Utils/CompositionalLayout.swift
+++ Terning-iOS/Terning-iOS/Resource/Utils/CompositionalLayout.swift
@@ -54,4 +54,56 @@ struct CompositionalLayout {
             }
         }
     }
+    
+    static func createCalendarListLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (_, _) -> NSCollectionLayoutSection? in
+            
+            let item = NSCollectionLayoutItem(
+                layoutSize: .init(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(100)
+                )
+            )
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 12)
+            
+            let group = NSCollectionLayoutGroup.vertical(
+                layoutSize: .init(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(100)
+                ),
+                subitems: [item]
+            )
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = 12 // 그룹 간의 간격 설정
+            
+            let headerSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(52)
+            )
+            let header = NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: headerSize,
+                elementKind: UICollectionView.elementKindSectionHeader,
+                alignment: .top
+            )
+            
+            header.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0)
+            
+            let footerSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(4)
+            )
+            let footer = NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: footerSize,
+                elementKind: UICollectionView.elementKindSectionFooter,
+                alignment: .bottom
+            )
+            
+            section.boundarySupplementaryItems = [header, footer]
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 12, trailing: 0)
+            
+            return section
+        }
+    }
 }

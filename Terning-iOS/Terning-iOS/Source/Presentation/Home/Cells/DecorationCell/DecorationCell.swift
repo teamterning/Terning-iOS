12ead8caed10ecd5d646e3822800bc09fd51174a
minKim-dev <lovejd0815@gmail.com>
2024-07-16 06:51:55 +0900

    [Remove] #49 - 컴포지셔널 레이아웃 구조 변경 후 필요없는 파일을 삭제했습니다.

--- Terning-iOS/Terning-iOS/Source/Presentation/Home/Cells/DecorationCell/DecorationCell.swift
+++ Terning-iOS/Terning-iOS/Source/Presentation/Home/Cells/DecorationCell/DecorationCell.swift
@@ -1,51 +1,49 @@
+////
+////  DecorationCell.swift
+////  Terning-iOS
+////
+////  Created by 김민성 on 7/10/24.
+////
 //
-//  DecorationCell.swift
-//  Terning-iOS
+//import UIKit
 //
-//  Created by 김민성 on 7/10/24.
+//import SnapKit
+//import Then
 //
-
-import UIKit
-
-import SnapKit
-import Then
-
-class DecorationCell: UICollectionViewCell {
-    
-    // MARK: - Properties
-    
-    // MARK: - UIComponents
-    
-    let decorationView = UIView().then {
-        $0.backgroundColor = .grey150
-    }
-    
-    // MARK: - LifeCycles
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        setHierarchy()
-        setLayout()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-// MARK: - UI & Layout
-
-extension DecorationCell {
-    func setHierarchy() {
-        contentView.addSubview(decorationView)
-    }
-    
-    func setLayout() {
-        decorationView.snp.makeConstraints {
-            $0.top.equalToSuperview()
-            $0.horizontalEdges.equalToSuperview()
-            $0.height.equalTo(4)
-        }
-    }
-}
+//class DecorationCell: UICollectionViewCell {
+//    
+//    // MARK: - UIComponents
+//    
+//    let decorationView = UIView().then {
+//        $0.backgroundColor = .grey150
+//    }
+//    
+//    // MARK: - LifeCycles
+//    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        
+//        setHierarchy()
+//        setLayout()
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//}
+//
+//// MARK: - UI & Layout
+//
+//extension DecorationCell {
+//    func setHierarchy() {
+//        contentView.addSubview(decorationView)
+//    }
+//    
+//    func setLayout() {
+//        decorationView.snp.makeConstraints {
+//            $0.top.equalToSuperview()
+//            $0.horizontalEdges.equalToSuperview()
+//            $0.height.equalTo(4)
+//        }
+//    }
+//}

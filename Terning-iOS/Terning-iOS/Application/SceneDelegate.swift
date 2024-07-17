bde89817080a1e9895b8a5346545552698de872d
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-07-17 13:12:16 +0900

    [Feat] #62 - 스크랩 서버 통신 구현

--- Terning-iOS/Terning-iOS/Application/SceneDelegate.swift
+++ Terning-iOS/Terning-iOS/Application/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let vc = LoginViewController(viewModel: LoginViewModel())
+        let vc = MainHomeViewController()
         window.rootViewController = vc
         self.window = window
         window.makeKeyAndVisible()

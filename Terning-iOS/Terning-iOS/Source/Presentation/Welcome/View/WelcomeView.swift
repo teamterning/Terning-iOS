04e27b25e54a2eca3c89c886b14ca372d7d124d2
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-09-11 16:07:51 +0900

    [Fix] #152 - 온보딩 이후에 뜨는 WelcomView UX 라이팅 수정

--- Terning-iOS/Terning-iOS/Source/Presentation/Welcome/View/WelcomeView.swift
+++ Terning-iOS/Terning-iOS/Source/Presentation/Welcome/View/WelcomeView.swift
@@ -55,7 +55,7 @@ extension WelcomeView {
             logoImageView.image = .imgOnbording
             startButton.setTitle(title: "시작하기")
         case .second:
-            welcomeLabel.text = "이제 딱 맞는 공고와 함께\n터닝을 시작해 볼까요?"
+            welcomeLabel.text = "나에게 딱 맞는 공고가 준비됐어요!\n터닝을 시작해 볼까요?"
             logoImageView.image = .imgwelcomTh1Ngjin
             startButton.setTitle(title: "내 맞춤 공고 바로 보러가기")
         }

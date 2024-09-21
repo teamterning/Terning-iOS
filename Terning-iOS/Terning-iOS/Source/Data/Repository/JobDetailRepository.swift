ad2498152b32ef0bbea28b44657cd0510e38dd08
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-09-22 01:18:17 +0900

    [Add] #177 - JobDetailRepository 파일 추가

---
+++ Terning-iOS/Terning-iOS/Source/Data/Repository/JobDetailRepository.swift
@@ -0,0 +1,8 @@
+//
+//  JobDetailRepository.swift
+//  Terning-iOS
+//
+//  Created by 이명진 on 9/22/24.
+//
+
+import Foundation
---
+++ Terning-iOS/Terning-iOS/Source/Domain/RepositoryInterface/JobDetailRepositoryInterface.swift
@@ -0,0 +1,8 @@
+//
+//  JobDetailRepositoryInterface.swift
+//  Terning-iOS
+//
+//  Created by 이명진 on 9/22/24.
+//
+
+import Foundation

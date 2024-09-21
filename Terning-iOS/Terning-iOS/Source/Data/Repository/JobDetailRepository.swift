fd2e4032de8180045dee83afa9deba611d462a7a
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-09-22 01:19:19 +0900

    [Feat] #177 - JobDetailRepository 모듈 구현

--- Terning-iOS/Terning-iOS/Source/Data/Repository/JobDetailRepository.swift
+++ Terning-iOS/Terning-iOS/Source/Data/Repository/JobDetailRepository.swift
@@ -5,4 +5,26 @@
 //  Created by 이명진 on 9/22/24.
 //
 
-import Foundation
+import RxSwift
+
+final class JobDetailRepository: JobDetailRepositoryInterface {
+    
+    private let scrapService: ScrapsServiceProtocol
+    
+    init(scrapService: ScrapsServiceProtocol) {
+        self.scrapService = scrapService
+    }
+    
+    func addScrap(internshipAnnouncementId: Int, color: String) -> Observable<Void> {
+        return scrapService.addScrap(internshipAnnouncementId: internshipAnnouncementId, color: color)
+    }
+    
+    func patchScrap(internshipAnnouncementId: Int, color: String) -> Observable<Void> {
+        return scrapService.patchScrap(internshipAnnouncementId: internshipAnnouncementId, color: color)
+    }
+    
+    func cancelScrap(internshipAnnouncementId: Int) -> Observable<Void> {
+        return scrapService.cancelScrap(internshipAnnouncementId: internshipAnnouncementId)
+    }
+}
+

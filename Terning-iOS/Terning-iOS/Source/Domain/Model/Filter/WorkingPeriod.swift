3f8f37cb0470957d49971c73b16d51df8266c370
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-12-28 10:54:52 +0900

    [Feat] #230 - 필터링 타입, 모델 정의

---
+++ Terning-iOS/Terning-iOS/Source/Domain/Model/Filter/WorkingPeriod.swift
@@ -0,0 +1,40 @@
+//
+//  WorkingPeriod.swift
+//  Terning-iOS
+//
+//  Created by 정민지 on 12/28/24.
+//
+
+import Foundation
+
+enum WorkingPeriod: String, CaseIterable, Codable {
+    case short = "1개월 ~ 3개월"
+    case middle = "4개월 ~ 6개월"
+    case long = "7개월 이상"
+    
+    var displayName: String {
+        return self.rawValue
+    }
+    
+    var englishValue: String {
+        switch self {
+        case .short:
+            return "short"
+        case .middle:
+            return "middle"
+        case .long:
+            return "long"
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .short:
+            return "짧은 기간 안에 유의미한 스펙을 쌓을 수 있어요!"
+        case .middle:
+            return "회사와 직무에 대해 이해하기 적당한 기간이에요!"
+        case .long:
+            return "오랜 기간 내 커리어에 맞는 직무경험을 만들 수 있어요!"
+        }
+    }
+}

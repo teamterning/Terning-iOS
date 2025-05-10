29082c8c131e440f0a2dca2ddaf7d25265121a82
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-05-10 22:35:30 +0900

    [Feat] #284 -  FCM 푸시알림 세팅 및 APNs, 권한 요청, 푸시 이벤트 처리 로직 구현

--- Terning-iOS/Terning-iOS/Application/AppDelegate.swift
+++ Terning-iOS/Terning-iOS/Application/AppDelegate.swift
@@ -23,27 +23,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         KakaoSDK.initSDK(appKey: Config.KakaoAppKey)
         // 파이어베이스 설정
         FirebaseApp.configure()
-
+        
         setupFCM(application)
         
         return true
     }
-    
-    private func setupFCM(_ application: UIApplication) {
-        // 파이어베이스 Meesaging 설정
-        Messaging.messaging().delegate = self
-        // 앱 실행 시 사용자에게 알림 허용 권한을 받음
-        UNUserNotificationCenter.current().delegate = self
-        // 필요한 알림 권한을 설정
-        UNUserNotificationCenter.current().requestAuthorization(options: [.sound, .alert, .badge]) { isAgree, error in
-            if isAgree {
-                print("알림허용")
-            }
-        }
-        // UNUserNotificationCenterDelegate를 구현한 메서드를 실행시킴
-        application.registerForRemoteNotifications()
-    }
-    
     
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         // 세로방향 고정
@@ -62,53 +46,94 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }    
-    
+    }
 }
 
+
+// MARK: - UNUserNotificationCenterDelegate
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
     
     // 백그라운드에서 푸시 알림을 탭했을 때 실행
     func application(_ application: UIApplication,
                      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        print("APNS token: \(deviceToken)")
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let tokenString = tokenParts.joined()
+        print("🔐 APNs 기기 토큰(token) : \(tokenString)")
+        
         Messaging.messaging().apnsToken = deviceToken
     }
     
-    /// 푸시클릭시
+    /// 푸시 클릭시
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
-        print("🟢", #function)
+        guard UserManager.shared.isPushEnabled ?? true else {
+            print("❌ 사용자가 푸시 OFF 설정함 → 무시")
+            return
+        }
+        
+        let userInfo = response.notification.request.content.userInfo
+        if let type = userInfo["type"] as? String {
+            PushNavigator.handlePush(type: type)
+        }
     }
     
-    // Foreground(앱 켜진 상태)에서도 알림 오는 설정
+    /// Foreground(앱 켜진 상태)에서도 알림 오는 설정
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.sound, .list, .banner])
+        guard UserManager.shared.isPushEnabled ?? true else {
+            print("❌ Foreground 푸시 무시됨")
+            completionHandler([]) // 아무것도 표시하지 않음
+            return
+        }
+        completionHandler([.sound, .banner])
     }
     
     /// error발생시
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("🟢", error)
+        print("🟢 [didFailToRegisterForRemoteNotificationsWithError] Error: 발생 ", error)
+    }
+    
+    /// FCM 세팅 메서드
+    private func setupFCM(_ application: UIApplication) {
+        // Firebase Messaging 설정
+        Messaging.messaging().delegate = self
+        
+        // 푸시 알림 델리게이트 설정
+        UNUserNotificationCenter.current().delegate = self
+        
+        // 권한 요청 및 설정 상태 반영
+        UNUserNotificationCenter.current().requestAuthorization(options: [.sound, .alert, .badge]) { isGranted, error in
+            DispatchQueue.main.async {
+                if isGranted {
+                    print("🔔 알림 허용됨")
+                    application.registerForRemoteNotifications()
+                } else {
+                    print("🚫 알림 거부됨")
+                }
+                
+                // ✅ 권한 상태를 UserManager와 동기화
+                UserManager.shared.isPushEnabled = isGranted
+            }
+        }
     }
 }
 
-extension AppDelegate: MessagingDelegate {
+// MARK: - MessagingDelegate
 
+extension AppDelegate: MessagingDelegate {
+    
     /// FCMToken 업데이트시
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        print("Firebase registration token: \(String(describing: fcmToken))")
-
+        print("🟢 Firebase 등록 토큰(token): \(String(describing: fcmToken))")
+        
+        UserManager.shared.fcmToken = fcmToken
+        
         let dataDict: [String: String] = ["token": fcmToken ?? ""]
         NotificationCenter.default.post(
-          name: Notification.Name("FCMToken"),
-          object: nil,
-          userInfo: dataDict
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
         )
         // TODO: If necessary send token to application server.
         // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
-
-    
 }
-
-
